e397b4c6c3704dbc22b629cb2acd2046197a6866
jabuile tovar <jabuiletovar@gmail.com>
2023-08-15 22:48:49 -0300

    segunta entrega a terminar

---
+++ Entregable2/producMD.js
@@ -0,0 +1,68 @@
+const fs = require ('fs');
+class ProductManager {
+    products;
+
+        constructor(){
+            this.products =[];
+            this.path = 'pro.txt';
+        }
+
+        getProducts(){
+            return this.products;
+        }
+
+        getProductById(id){
+            let productId= this.products.find(p=>p.id==id);
+            if (productId){
+                return console.log(productId);
+                
+            }else{
+                console.log("Not  found")
+            }
+
+        }
+
+
+        async addProduct(title, description,price, thumbnail,code,stock) {    
+            if(this.products.length > 0){
+                for(let productB of this.products){
+                    if(code===productB.code){
+                    /*return*/ console.log("codigo existente"); //ver si necesita o no return 
+                    }
+                }
+            }
+
+            if (!title || !description || !price || !thumbnail || !code || !stock){
+                console.error("Campo incompleto");
+                return
+            }
+
+            let id
+            if (this.products.length === 0){
+                id=1;
+            }else{
+                id=this.products.length + 1;
+            }
+
+            const newProduct ={
+                id,
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock,
+            }
+            this.products.push(newProduct);
+            await fs.promises.writeFile(this.path, JSON.stringify(newProduct))
+        }
+
+
+}
+
+const product1 =new ProductManager();
+product1.addProduct("herramienta","martillo", 10,"x", 2,5);
+product1.addProduct("herramienta","pala", 50,"y", 3,6);
+product1.addProduct("herramienta","pala", 50,"y", 6);
+console.log(product1.getProducts())
+product1.getProductById(2);
